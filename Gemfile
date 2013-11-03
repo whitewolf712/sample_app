dcd86fda093ad0612ca5d3acafb17fe25618540b
Evgeniy Ponomarenko <whitewolf@dbds.ru>
2013-11-04 00:01:20 +0700

    Add Users (and password)

--- Gemfile
+++ Gemfile
@@ -2,7 +2,8 @@ source 'https://rubygems.org'
 
 gem 'rails'
 gem 'bootstrap-sass'
-gem 'bcrypt-ruby'
+gem 'bcrypt-ruby', '~> 3.0.0'
+
 
 group :development do
   gem 'annotate'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,27 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime
+#  updated_at      :datetime
+#  password_digest :string(255)
 #
 
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: 'Example User', email: 'user@example.com') }
+  before { @user = User.new(name: 'Example User', email: 'user@example.com',
+                            password: 'foobar', password_confirmation: 'foobar' ) }
+
   subject { @user }
-  it { should  respond_to (:name) }
-  it { should  respond_to (:email) }
+  it { should respond_to (:name) }
+  it { should respond_to (:email) }
+  it { should respond_to (:password_digest) }
+  it { should respond_to (:password) }
+  it { should respond_to (:password_confirmation) }
+  it { should respond_to (:authenticate) }
 
   it { should be_valid }
 
@@ -39,7 +46,6 @@ describe User do
       addresses = %w[user@foo,com foo user_at_foo.ord example_user@foo.
                 foo@bar_baz.com foo@bar+baz.com]
       addresses.each do |invalid_address|
-        p @user
         @user.email = invalid_address
         @user.should_not be_valid
       end
@@ -53,6 +59,57 @@ describe User do
         @user.email = valid_address
         @user.should be_valid
       end
+    end
+  end
+
+  describe 'email address with mixed case' do
+    let(:mixed_case_email) { 'Foo@ExAMPle.CoM' }
+
+    it 'should be saved as all lower-case' do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
+  describe 'when email address is already taken' do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email.upcase!
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe 'when password is not present' do
+    before { @user.password = @user.password_confirmation = ' ' }
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = 'mismatch' }
+    it { should_not be_valid }
+  end
+
+  describe 'when password confirmation is nil' do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
+
+  describe 'return value of authenticate method' do
+    before { @user.save }
+    let(:found_user) { User.find_by_email(@user.email) }
+
+    describe 'with valid password' do
+      it { should == found_user.authenticate(@user.password) }
+    end
+
+    describe 'with invalid password' do
+      let(:user_for_invalid_password) { found_user.authenticate('invalid') }
+
+      it { should_not == user_for_invalid_password }
+      specify { user_for_invalid_password.should be_false }
     end
   end
 end
