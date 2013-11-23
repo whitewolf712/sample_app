f4ba2fee6675ce4bdfc93154ebba82b7c3163bdf
Evgeniy Ponomarenko <whitewolf@dbds.ru>
2013-11-23 23:32:40 +0700

    Add a sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,6 @@
 #
 
 class User < ActiveRecord::Base
-  has_secure_password
   before_save { email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -19,6 +18,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true
+  validates :password, length: { minimum: 6 }
+  # validates :password_confirmation, presence: true
+  has_secure_password
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,10 +13,14 @@ describe 'User Pages' do
     it { should have_selector('h1', text: 'Sign up') }
     it { should have_title(full_title('Sign up')) }
 
-    # FIXIT: undefined method `сlick_button'
     describe 'with invalid information' do
       it 'should not to create a user' do
         expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe 'after submission' do
+        before { click_button submit }
+        it { should have_css('div.alert-error') }
       end
     end
 
@@ -30,6 +34,14 @@ describe 'User Pages' do
 
       it 'should to create a user' do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe 'after saving the user' do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(full_title(user.name)) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome')}
       end
     end
   end
